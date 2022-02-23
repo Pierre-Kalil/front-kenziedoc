076d7fa69e24af202b7ce7631a1fd4ad943a9c19
Pierre Kalil <pierrekalil1@gmail.com>
2022-02-23 10:41:42 -0300

    feat: basic structure all appointments

--- src/pages/AllAppointments/index.tsx
+++ src/pages/AllAppointments/index.tsx
@@ -1,7 +1,97 @@
+import { useAppointment } from "../../context/Appointments";
+import { useAuth } from "../../context/Auth";
+import {
+  BoxAppointments,
+  CardAppointments,
+  ContainerAppointments,
+  HeaderProfAdmin,
+  Ul,
+  Li,
+} from "./style";
+
 export const AllAppointments = () => {
+  const { userType } = useAuth();
+  const { appointmentPatient, appointmentProf, tomorrow, waitList } =
+    useAppointment();
   return (
     <>
-      <h1>Consultas</h1>
+      {userType.length === 11 ? (
+        <ContainerAppointments>
+          <BoxAppointments>
+            {appointmentPatient.map((item, index) => (
+              <CardAppointments key={index}>
+                <span>{item.patient_name}</span>
+                <span>{item.date}</span>
+                <span>{item.professional.name}</span>
+                <span>{item.professional.specialty}</span>
+                <span>{item.finished}</span>
+              </CardAppointments>
+            ))}
+          </BoxAppointments>
+        </ContainerAppointments>
+      ) : (
+        <></>
+      )}
+      {userType.length > 5 ? (
+        <ContainerAppointments>
+          <HeaderProfAdmin>
+            <Ul>
+              <Li>
+                <button> Minhas consultas</button>
+              </Li>
+              <Li>
+                <button> Lista de espera </button>
+              </Li>
+            </Ul>
+          </HeaderProfAdmin>
+          <BoxAppointments>
+            {appointmentProf.map((item, index) => (
+              <CardAppointments key={index}>
+                <span>{item.date}</span>
+                <span>{item.patient.name}</span>
+                <span>{item.patient.age}</span>
+                <span>{item.patient.sex}</span>
+                <span>{item.patient.healt_plan}</span>
+                <span>{item.finished}</span>
+                <button>Finalizar</button>
+              </CardAppointments>
+            ))}
+          </BoxAppointments>
+        </ContainerAppointments>
+      ) : (
+        <></>
+      )}
+      {userType ? (
+        <ContainerAppointments>
+          <HeaderProfAdmin>
+            <Ul>
+              <Li>
+                <button>Consultas paciente</button>
+              </Li>
+              <Li>
+                <button>Consultas médico</button>
+              </Li>
+              <Li>
+                <button>Lista de espera médico</button>
+              </Li>
+              <Li>
+                <button>Consultas de amanhã</button>
+              </Li>
+            </Ul>
+          </HeaderProfAdmin>
+          <BoxAppointments>
+            {tomorrow.map((item, index) => (
+              <CardAppointments key={index}>
+                <span>{item.date}</span>
+                <span>{item.patient}</span>
+                <span>{item.professional}</span>
+              </CardAppointments>
+            ))}
+          </BoxAppointments>
+        </ContainerAppointments>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
---
+++ src/pages/AllAppointments/style.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ContainerAppointments = styled.div`
+  display: flex;
+  justify-content: center;
+
+  width: 80%;
+  height: 80vh;
+`;
+
+export const BoxAppointments = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const CardAppointments = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+  margin: 5px;
+`;
+
+export const HeaderProfAdmin = styled.div`
+  display: flex;
+  height: 10vh;
+  width: 100%;
+`;
+
+export const Ul = styled.ul`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Li = styled.li`
+  list-style: none;
+  margin: 5px;
+`;
